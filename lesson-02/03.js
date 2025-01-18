5073015c846a674db8bafff81585df0aca59267f
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-01-18 10:08:59 +0200

    complete HW lesson-02

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,18 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = 70; // тестовое значение, можно изменять
+let grade;
 // your code
+if (score >= 0) {
+    grade = 'F';
+} else if (score >= 50) {
+    grade = 'D';
+} else if (score >= 70) {
+    grade = 'C';
+} else if (score >= 80) {
+    grade = 'B';
+} else if (score >= 90){
+    grade = 'A';
+};
+
